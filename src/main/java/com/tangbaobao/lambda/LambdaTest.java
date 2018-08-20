4c1d59d451db007eb39893629f39b818e80bdf13
tangbaobao <tangbaoabo@nefu.edu.cn>
2018-08-20 22:48:38 +0800

    first commit

---
+++ src/main/java/com/tangbaobao/lambda/LambdaTest.java
@@ -0,0 +1,17 @@
+package com.tangbaobao.lambda;
+
+import org.junit.Test;
+
+/**
+ * 测试lambda表达式
+ *
+ * @author 唐学俊
+ * @create 2018/08/20
+ **/
+
+public class LambdaTest {
+    @Test
+    public void fun1() {
+
+    }
+}
